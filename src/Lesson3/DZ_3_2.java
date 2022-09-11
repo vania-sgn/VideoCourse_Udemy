30bb772d07c279c51dc8bef8b94faafd4cce3824
sklad <Skladchikov.vania@gmail.com>
2022-09-11 17:03:05 +0300

    VideoCourse

---
+++ src/Lesson3/DZ_3_2.java
@@ -0,0 +1,13 @@
+package Lesson3;
+
+public class DZ_3_2 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = a-- - --a + ++a + a++ + a;
+        System.out.println(b);
+
+        int  c = 8;
+        int d = ++b - b++ + ++b - --b;
+        System.out.println(d);
+    }
+}
--- src/Lesson3/Test3.java
+++ src/Lesson3/Test3.java
@@ -1,4 +1,23 @@
 package Lesson3;
 
 public class Test3 {
+    public static void main(String[] args) {
+      //  System.out.println("Video");
+
+        // Арифметические операторы
+        double a = 5;
+        double b =10;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+        System.out.println(a++);
+        System.out.println(--b);
+
+        // Операторы сравнения
+        // Операторы присваивания
+        // Логические операторы
+
+    }
 }
