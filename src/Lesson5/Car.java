1923d5e4f4fba8dc77c2cada5408eeff3c79adf5
sklad <Skladchikov.vania@gmail.com>
2022-09-24 22:57:40 +0300

    VideoCourse

---
+++ src/Lesson5/Car.java
@@ -0,0 +1,34 @@
+package Lesson5;
+
+public class Car {
+    String name;
+    String color;
+    double speed;
+
+    int gaz(int skorost) {
+        speed += skorost;
+        return skorost;
+    }
+
+    int tormoz(int skorost) {
+        speed -= skorost;
+        return skorost;
+    }
+
+    void showInfo() {
+        System.out.println("Marka- " + name + " cvet-" + color + " skorost-" + speed);
+    }
+}
+    class Car_Test{
+        public static void main(String[] args) {
+            Car c1 = new Car();
+            c1.name="bmv";
+            c1.speed=20.1;
+            c1.color= "White";
+            c1.gaz(20);
+            c1.showInfo();
+            c1.tormoz(10);
+            c1.showInfo();
+        }
+    }
+
